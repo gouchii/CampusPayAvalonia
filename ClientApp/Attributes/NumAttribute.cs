97932ed80cdb98057e73f687c8016fae72349654
CMV_10 <kidcaustic10@gmail.com>
2025-05-28 19:25:09 +0800

    Update NumAttribute.cs

--- ClientApp/Attributes/NumAttribute.cs
+++ ClientApp/Attributes/NumAttribute.cs
@@ -8,7 +8,21 @@ public class NumAttribute : ValidationAttribute
 {
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        throw new NotImplementedException();
+        if (value == null)
+        {
+            return new ValidationResult("Value cannot be null.");
+        }
+
+        if (value is decimal decimalValue)
+        {
+            if (decimalValue <= 0)
+            {
+                return new ValidationResult("Amount must be a positive number.");
+            }
+
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult("Invalid number format.");
     }
 }
-
